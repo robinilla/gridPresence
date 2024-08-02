62876168224343b7a8862363975f3313defedf36
robinilla <sonia.illanasc@me.com>
2024-08-02 20:19:08 +0200

    Update CountsSimulationAndTransformation.R

--- script/CountsSimulationAndTransformation.R
+++ script/CountsSimulationAndTransformation.R
@@ -3,117 +3,142 @@ library(terra)
 library(sf)
 library(ggpubr)
 library(tidyverse)
-
 rm(list=ls())
-set.seed(1993)
 
 # Hunting estates collected simulation
 # As collected hunting yields cannot be shared due to data share agreement conficendialty
 # Simulated hunting yields data sets are provided for running gridPresence() function 
 
-# load the grid/s
-# Read 5 x 5 km grid: downloaded from the EEA grid and masked with Spain
-load(url("https://raw.githubusercontent.com/robinilla/gridPresence/main/script/grid5km.Rdata")) 
+# ---------------------------------------------
+# 1) load the provided data example
+# ---------------------------------------------
+# it contains:
+# - a 5 x 5 km grid: downloaded from the EEA grid and masked with Spain
+# - a simulated polygon layer with three simulated counts of whatever species
+load(url("https://raw.githubusercontent.com/robinilla/gridPresence/main/script/data_example.Rdata")) 
 
-# Transform the 5 x 5 km grid to 10 x 10 km (EEA grid code )
-# it will take few minutes, be patient, it works
+# Transform the 5 x 5 km grid to 10 x 10 km, by the EEA grid code id
 grid10km<-grid5km %>% group_by(CELLCOD) %>% summarise(geometry = sf::st_union(geometry)) %>% ungroup()
 
 
-# Create hunting estates (irregular polygons ): from the extent of our 10x10 km grid 
-sarea <- rast(nrows = 100, ncols = 100, ext(grid10km))
-sarea<-raster(sarea)
-pp <- randomPoints(sarea, #introduce the layer
-                   2000)  #introduce number of irregular polygons to be created
-cotos <- crop(voronoi(pp), sarea) %>% st_as_sf()  %>% st_set_crs(st_crs(grid10km)) #transform as sf object with same crs as EEA grid
+# -----------------------------------------------
+# 2) gridPresence function: it needs 4 parameters
+# -----------------------------------------------
+# big = a sf object class
+# small = a sf object class
+# id = unique id column name from the big object class.
+# count = column name of count data from the small object class
+# Note 1: big and small are objects that must have the same coordinate reference
+# system, if not an ERROR will be display
+# Note 2: id and count are not objects of your environment. They are column 
+# names, and must not be provided between quotes. The function 
+# implements quotation of these parameters (line)
 
-# From the created hunting estates select only those ones that are inside of the 10 x 10 grid
-cotos.inside<-st_intersection(cotos, grid10km) %>% group_by(id) %>% summarize(contar=1) %>% ungroup()
+# small<-polygon.layer
+# big<-grid5km
+# id<-quo(CELLCOD)
+# count<-quote(spYear1)
 
-hunting.seasons<-5
-cotos.hs<-list()
-
-#Create a hunting bag for each hunting estate and hunting season
-for (i in 1:hunting.seasons) {
-  # Create a hunting bag as a random poisson process with lambda
-  lambda<-rnorm(1, mean=50, sd=10)
-  cotos.inside<-cotos.inside %>% mutate(HB=rpois(cotos.inside %>% nrow(), lambda), id=row_number())
+# Create the function with the 4 above commented parameters
+gridPresence<-function(small, big, id, count){                              
   
-  # The 25 percent of the hunting estates are selecting for changing their hunting yield to 0: unknown or not reported information of the hunting estate
-  cotos0<-cotos.inside %>% sample_frac(.25, replace = FALSE) %>% mutate(HB=0)
-  cotoshb<-cotos.inside[!(cotos.inside$id %in% cotos0$id),]
-  cotosf<-rbind(cotos0, cotoshb)
+  # Check if small parameter is a sf object 
+  if(!("sf" %in% class(small))) {                                             
+    stop("ERROR: Class of first argument is not sf")
+  }
+  # Check if big parameter is a sf object
+  if(!("sf" %in% class(big))) {                                               
+    stop("ERROR: Class of second argument is not sf")
+  }
   
-  cotos.hs[[i]]<-cotosf %>% mutate(hs=paste("HS", i, sep=""))
+  # if(!(id %in% colnames(big))) {
+  #   stop("ERROR: Class of third argument is not a column name of the big layer")
+  # }
+  # if(!(count %in% colnames(small))) {
+  #   stop("ERROR: Class of forth argument is not a column name of the small layer")
+  # }
+  
+  # Check that big and small objects have the same Coordinate Reference System
+  if (st_crs(big)!=st_crs(small)) {                                           
+    stop ("CRS is different in both layers. Please transform one to have the same")
+  } else {
+    # Quote the name of count column (from small layer)
+    count<-enquo (count)                                                  
+    # Quote the name of id column (from big layer)
+    id<-enquo (id)
+    
+    # Make the intersection of all grids with all hunting grounds
+    # it returns the same cell grid as many times as a cell intersects with a 
+    # polygon, acquiring the cell grid the information of each polygon 
+    # feature with whom it intersects
+    intersection<- st_join(big, small)
+    
+    # Group all cell grids with the same id from the above intersection
+    intersection<-intersection %>% 
+      # Transform count to presence: if count field is NA or 0, it will be 0,
+      #  and if count field is above 0 it will be 1
+      mutate(Presence=ifelse(is.na(!!count), 0, ifelse(!!count>0, 1, 0))) %>% 
+      # transform into a tibble for grouping faster
+      as_tibble() %>% 
+      # use id for grouping all the cell grids
+      group_by(!!id) %>% 
+      # sum presence values for each same grid id 
+      summarize(Presence=sum(Presence)) %>% 
+      # retransform presence sum values to 1 or 0
+      mutate(Presence=as.numeric(ifelse(Presence>0, 1, 0)))
+    
+    # join the big layer with the resulting intersection object for returning
+    # the fields of the big layer and the new presence field created
+    intersection<-big %>% left_join(intersection, by=quo_name(id))
+  }
+  return(intersection)
 }
 
-cotos.hs<-spread(do.call(rbind, cotos.hs), hs, HB) %>%  #join all the hunting seasons into a same object and transform it from long to wide format 
-  mutate(species="Sus scrofa") # We use wild boar species as example, but hunting bags, or counts data could be for any species
 
-# Plot collected hunting yields for the different hunting seasons
-ggarrange(ggplot()+
-            geom_sf(data=cotos.hs, aes(fill=HS1))+ 
-            scale_fill_distiller()+
-            geom_sf(data=cotos.hs %>% filter(HS1==0), color="yellow", fill=NA)+
-            theme_bw(),
-          ggplot()+
-            geom_sf(data=cotos.hs, aes(fill=HS2))+ 
-            scale_fill_distiller()+
-            geom_sf(data=cotos.hs %>% filter(HS2==0), color="yellow", fill=NA)+
-            theme_bw(),
-          ggplot()+
-            geom_sf(data=cotos.hs, aes(fill=HS3))+ 
-            scale_fill_distiller()+
-            geom_sf(data=cotos.hs %>% filter(HS3==0), color="yellow", fill=NA)+
-            theme_bw(), 
-          ggplot()+
-            geom_sf(data=cotos.hs, aes(fill=HS4))+ 
-            scale_fill_distiller()+
-            geom_sf(data=cotos.hs %>% filter(HS4==0), color="yellow", fill=NA)+
-            theme_bw(),
-          ggplot()+
-            geom_sf(data=cotos.hs, aes(fill=HS5))+ 
-            scale_fill_distiller()+
-            geom_sf(data=cotos.hs %>% filter(HS5==0), color="yellow", fill=NA)+
-            theme_bw(),
-          nrow=3, ncol=2, common.legend=TRUE)
-
-
-
-#load the function for transforming hunting bags data to presence in a grid 
-source("https://raw.githubusercontent.com/robinilla/gridPresence/main/script/gridPresence.R")
-
-
-#Make the grid transformation from different hunting seasons at once
+# Make the grid transformation from different years and different grids at once
 intersection.list10km<-list()
 intersection.list5km<-list()
-for (i in 1:(cotos.hs %>% dplyr::select(matches("HS")) %>% ncol() -1)){    # take care that the hunting yields from different hunting seasons have some common name in the columns name
-  col.name<-(cotos.hs %>% dplyr::select(matches("HS")) %>% colnames())[i]
+for (i in 1:(polygon.layer %>% dplyr::select(matches("sp")) %>% ncol() -1)){    # take care that the hunting yields from different hunting seasons have some common name in the columns name
+  col.name<-(polygon.layer %>% dplyr::select(matches("sp")) %>% colnames())[i]
   print(col.name)                                                          # just to know which hunting season we are running
-  layer.sp1<-cotos.hs %>% dplyr::select(id, species, all_of(col.name))
-  intersection.list10km[[i]]<-gridPresence(small=layer.sp1, big=grid10km, id=CELLCOD, harvTot=!!sym(col.name))
-  intersection.list5km[[i]]<-gridPresence(small=layer.sp1, big=grid5km, id=cellcode, harvTot=!!sym(col.name))
+  layer.sp1<-polygon.layer %>% dplyr::select(id, all_of(col.name))
+  intersection.list10km[[i]]<-gridPresence(small=layer.sp1, big=grid10km, id=CELLCOD, count=!!sym(col.name))
+  intersection.list5km[[i]]<-gridPresence(small=layer.sp1, big=grid5km, id=cellcode, count=!!sym(col.name))
 }
 
-#Plot grid presence: plot example for first hunting season
+#Plot grid presence: plot example for first count year
 ggarrange(ggplot()+
             geom_sf(data=intersection.list10km[[1]], aes(fill=as.factor(Presence), alpha=0.25), color="darkgrey")+   # 10 x 10 km grid
             scale_fill_manual(values = c("#d3d3d3", "#6eac5c"))+
-            # geom_sf(data=cotos.hs %>% filter(HS1==0), color="darkred", fill="#d3d3d3", alpha=0.3)+
-            # geom_sf(data=cotos.hs %>% filter(HS1!=0), color="black", fill="#6eac5c", alpha=0.15)+
-            # geom_sf(data=layer.sp1 %>% filter(Presence==1), aes(fill=Presence))+
             ggtitle("10 x 10 km")+
             theme(legend.position = "none")+
             theme_bw(),
           ggplot()+
             geom_sf(data=intersection.list5km[[1]], aes(fill=as.factor(Presence), alpha=0.25), color="darkgrey")+   # 10 x 10 km grid
             scale_fill_manual(values = c("#d3d3d3", "#6eac5c"))+
-            # geom_sf(data=cotos.hs %>% filter(HS1==0), color="darkred", fill="#d3d3d3", alpha=0.3)+
-            # geom_sf(data=cotos.hs %>% filter(HS1!=0), color="black", fill="#6eac5c", alpha=0.15)+
-            # geom_sf(data=layer.sp1 %>% filter(Presence==1), aes(fill=Presence))+
             ggtitle("5 x 5 km")+
             theme(legend.position = "none")+
             theme_bw(), 
           ncol=2, nrow=1, common.legend = T, legend = "none")
 
 
+
+# Plot collected hunting yields for the different hunting seasons
+# uncomment lines 27-42 for running
+# yellow line polygons represent the value 0, that is, no individuals have been counted
+# ggarrange(ggplot()+
+#             geom_sf(data=polygon.layer, aes(fill=spYear1))+ 
+#             scale_fill_distiller()+
+#             geom_sf(data=polygon.layer %>% filter(spYear1==0), color="yellow", fill=NA)+
+#             theme_bw(),
+#           ggplot()+
+#             geom_sf(data=polygon.layer, aes(fill=spYear2))+ 
+#             scale_fill_distiller()+
+#             geom_sf(data=polygon.layer %>% filter(spYear2==0), color="yellow", fill=NA)+
+#             theme_bw(),
+#           ggplot()+
+#             geom_sf(data=polygon.layer, aes(fill=spYear3))+ 
+#             scale_fill_distiller()+
+#             geom_sf(data=polygon.layer %>% filter(spYear3==0), color="yellow", fill=NA)+
+#             theme_bw(), 
+#           nrow=3, ncol=1, common.legend=F)
